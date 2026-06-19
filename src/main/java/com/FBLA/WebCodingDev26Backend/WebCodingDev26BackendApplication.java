340428f380d1de604b3940df9df8f0a778ecda9b
Sashwat Choudhary <sashair@Sashwats-MacBook-Air.local>
2026-06-18 21:11:27 -0500

    first commit

---
+++ src/main/java/com/FBLA/WebCodingDev26Backend/WebCodingDev26BackendApplication.java
@@ -0,0 +1,13 @@
+package com.FBLA.WebCodingDev26Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCodingDev26BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebCodingDev26BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FBLA/WebCodingDev26Backend/WebCodingDev26BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FBLA.WebCodingDev26Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCodingDev26BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
